13140e905b44af7849eb29660eab5fa4c99fa283
SURESHDOMA <69568120+sureshdoma123@users.noreply.github.com>
2021-05-12 08:25:58 +0530

    Add files via upload

---
+++ happynumber.cpp
@@ -0,0 +1,31 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{
+	int n,s=0,r;
+	cin>>n;
+	while(n>9)
+	{
+		while(n)
+		{
+			r=n%10;
+			n=n/10;
+			s+=r*r;
+		}
+		n=s;
+		s=0;
+	}
+	if(n==0)
+	{
+		n=s;
+	}
+	if((n==1 || n==7) && n!=4)
+	{
+		cout<<"Happy Number";
+	}
+	else
+	{
+		cout<<"Not a Happy Number";
+	}
+	return 0;
+}
