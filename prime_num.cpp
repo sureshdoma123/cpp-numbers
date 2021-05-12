78a143ba3fea6e185271c27c0e65cb5f93279771
SURESHDOMA <69568120+sureshdoma123@users.noreply.github.com>
2021-05-12 18:14:21 +0530

    Update prime_num.cpp

--- prime_num.cpp
+++ prime_num.cpp
@@ -1,16 +1,39 @@
+//Method 1//
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
 	int n,c=0;
 	cin>>n;
-	for(int i=2;i<=n/2+1;i++)
+	for(int i=1;i<=n;i++)
+	{
+		if(n%i==0)
+			c++;
+	}
+	if(c==2)
+		cout<<"Prime";
+	else
+		cout<<"Not prime";
+}
+
+//Method 2//
+#include<bits/stdc++.h>
+using namepace std;
+main()
+{
+	int c=0,n;
+	cout<<"Enter a Number: ";
+	for(int i=2;i<=(n/2)+1;i++)
 	{
 		if(n%i==0)
 			c++;
 	}
 	if(c==1)
-		cout<<"Prime";
+		cout<<"Prime number";
 	else
-		cout<<"Not prime";
+		cout<<"Not a Prime number";
+	return 0;
 }
+
+//Method 3//
+#include<bits\
