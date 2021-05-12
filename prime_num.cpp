9c19e23d76639cc4ad932edc7068b3389b391393
SURESHDOMA <69568120+sureshdoma123@users.noreply.github.com>
2021-05-12 18:23:30 +0530

    Update prime_num.cpp

--- prime_num.cpp
+++ prime_num.cpp
@@ -1,39 +1,66 @@
-//Method 1//
-#include<bits/stdc++.h>
+//Method 1
+
+#include<bits\stdc++.h>
 using namespace std;
 main()
 {
-	int n,c=0;
+	int n,c=0,i=2;
+	cout<<"Enter a number: ";
 	cin>>n;
-	for(int i=1;i<=n;i++)
+	while(i>=n-1)
 	{
 		if(n%i==0)
 			c++;
+			break;
+		i++;
 	}
-	if(c==2)
-		cout<<"Prime";
-	else
-		cout<<"Not prime";
-}
-
-//Method 2//
-#include<bits/stdc++.h>
-using namepace std;
-main()
-{
-	int c=0,n;
-	cout<<"Enter a Number: ";
-	for(int i=2;i<=(n/2)+1;i++)
-	{
-		if(n%i==0)
-			c++;
-	}
-	if(c==1)
+	if(c==0)
 		cout<<"Prime number";
 	else
 		cout<<"Not a Prime number";
-	return 0;
 }
 
-//Method 3//
-#include<bits\
+//Method 2
+
+#include<bits\stdc++.h>
+using namespace std;
+main()
+{
+	int n,c=1,i=1;
+	cout<<"Enter a number: ";
+	cin>>n;
+	while(i>=(n/2))
+	{
+		if(n%i==0)
+			c++;
+			break;
+		i++;
+	}
+	if(c==1)
+		cout<<"Prime Number";
+	else
+		cout<<"Not a Prime Number";
+}
+
+//Method 3
+
+#include<bits\stdc++.h>
+using namespace std;
+main()
+{
+	int n,c=0,i=2;
+	cout<<"Enter a Number: ";
+	cin>>n;
+	while(i<=sqrt(n))
+	{
+		if(n%i==0)
+			c++;
+			break;
+		i++;
+	}
+	if(c==0)
+		cout<<"Prime NUmber";
+	else
+		cout<<"Not a Prime Number";
+	
+}
