d3825ab5d938d46a371c2cde31287c22cf85873c
SURESHDOMA <69568120+sureshdoma123@users.noreply.github.com>
2021-05-12 18:37:40 +0530

    Add files via upload

---
+++ inputoutput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string fname,lname;
+    cout<<"Enter first name: ";
+    cin>>fname;
+    cout<<"Enter last name: ";
+    cin>>lname;
+    cout<<fname+" "+lname;
+    return 0;
+
+}
